60f60bbad68032070d41192ea77570e19bc49cf2
anbya <anbyaalibia@gmail.com>
2019-09-30 17:33:09 +0700

    a

--- controllers/content.js
+++ controllers/content.js
@@ -13,7 +13,7 @@ module.exports = {
         Content
         .find()
         .populate("contentImg", "contentimages")
-        .populate("contentUser", "users")
+        // .populate("contentUser", "users")
         .then((error,result)=>{
             if (error){
                 res.status(400).send({
